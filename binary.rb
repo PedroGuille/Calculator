854e522d729b97746c69a4b1d84d81dc3a5fb9ef
PedroGuille <pedroguillermo1998@gmail.com>
2016-05-04 20:02:30 -0300

    Update binary.rb

--- binary.rb
+++ binary.rb
@@ -1,7 +1,14 @@
 require_relative "operation.rb"
 ﻿class Binary < Operation
-attr_accesor :operand_1, :operand_2
-def Binary
-
-end
+  attr_accesor :operand_1, :operand_2
+  def add_operand(parametr_op)
+﻿     if @operand_1.nil?
+﻿       @operand_1=parametr_op
+﻿     else
+﻿       @operand_2=parametr_op
+﻿     end
+   
+    
+  end
+  
 end
